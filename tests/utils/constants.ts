576a46f68c4daaf5a7b83975b0ade45139763aac
Jason Watts <jasonwdot@gmail.com>
2025-10-30 15:42:51 -0600

    added constants

---
+++ tests/utils/constants.ts
@@ -0,0 +1,19 @@
+//URLs
+export const BASE_URL = "https://www.saucedemo.com/";
+export const LOGIN_URL = `${BASE_URL}login`;
+export const INVENTORY_URL = `${BASE_URL}inventory.html`;
+export const CHECKOUT_URL = `${BASE_URL}checkout-step-one.html`;
+
+//Usernames
+export const USERNAME_STANDARD = "standard_user";
+export const USERNAME_LOCKED_OUT = "locked_out_user";
+export const USERNAME_PROBLEM = "problem_user";
+export const USERNAME_PERFORMANCE = "performance_glitch_user";
+export const USERNAME_EMPTY = "";
+export const USERNAME_ERROR = "error_user";
+export const USERNAME_VISUAL = "visual_user";
+
+//Passwords
+export const PASSWORD = "secret_sauce";
+export const INVALID_USERNAME = "invalid_user";
+export const INVALID_PASSWORD = "invalid_password";
